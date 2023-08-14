5d34b403e70eb3893cdab8c3e61cd12acf06aefc
Gaurav Choudhary <gloonaich@gmail.com>
2023-08-14 15:52:00 +0530

    adjusted landing page

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -9,21 +9,79 @@ const Landing = ({ setSelectedPage }) => {
       id="home"
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
     >
-      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:-32">
+      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:-32 mx-5">
         {isAboveMediumScreens ? (
           <div
             className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
-            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+            before:w-[60%] before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
               src="src/assets/profile-image.png"
               alt="profile"
-              className="hover:filter hoversaturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-[70%] max-w-[400px] md:max-w-[600px]"
             />
           </div>
         ) : (
-          <div></div>
+          <img
+            src="src/assets/profile-image.png"
+            alt="profile"
+            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+          />
         )}
+      </div>
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32 mx-16">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-7xl font-playfair z-10 text-center md:text-start">
+            Gaurav {""}
+            <div
+              className="my-6 xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1] "
+            >
+              Choudhary
+            </div>
+          </p>
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
+            viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+          </p>
+        </motion.div>
+        {/* Call actions */}
+        <motion.div
+          className="flex mt-5 justify-centermd:jutify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-y px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue"
+            onClick={() => setSelectedPage("contact")}
+          >
+            <div>Let's Talk</div>
+          </AnchorLink>
+        </motion.div>
       </div>
     </section>
   );
